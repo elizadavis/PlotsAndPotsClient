7225455ca5e1cd59f893ec6ce039020a9e8d7be5
elizadavis <eelizadavis@gmail.com>
2019-06-07 16:10:19 -0400

    Authorization Working
    Sign Up, Sign In, Sign Out, Change PW working

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const store = require('../store')
+
+const onSignUpSuccess = responseData => {
+  $('#message').html('signed up successfully!')
+  $('#sign-up input[type="email"]').val('')
+  $('#sign-up input[type="password"]').val('')
+}
+
+const onSignUpFailure = responseData => {
+  $('#message').html('sign up failed')
+  $('#sign-up input[type="email"]').val('')
+  $('#sign-up input[type="password"]').val('')
+}
+
+const onSignInSuccess = responseData => {
+  $('#message').html('signed in')
+  $('#signup-in').hide()
+  $('#loggedin').removeClass('hidden')
+  $('#sign-in input[type="email"]').val('')
+  $('#sign-in input[type="password"]').val('')
+  store.user = responseData.user
+}
+
+const onSignInFailure = responseData => {
+  $('#message').html('sign in failed')
+  $('#sign-in input[type="email"]').val('')
+  $('#sign-in input[type="password"]').val('')
+}
+
+const onChangePasswordSuccess = () => {
+  $('#message').html('password changed successfully!')
+  $('#change-pw input[type="password"]').val('')
+}
+
+const onChangePasswordFailure = () => {
+  $('#message').html('unable to change password')
+  $('#change-pw input[type="password"]').val('')
+}
+
+const onSignOutSuccess = () => {
+  $('#message').html('signed out successfully')
+  $('#signup-in').show()
+  $('#game-board').addClass('hidden')
+  $('#loggedin').addClass('hidden')
+}
+
+const onSignOutFailure = () => {
+  $('#message').html('sign out failed')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
