4a18cb121bc12b2f6f5f49d595e17e46536c262a
elizadavis <eelizadavis@gmail.com>
2019-06-11 08:46:44 -0400

    Update onIndexSuccess with handlebars
    Modified onIndexSuccess to use handlebars template

--- assets/scripts/plants/ui.js
+++ assets/scripts/plants/ui.js
@@ -5,18 +5,8 @@ const showPlantsTemplate = require('../templates/plant-listing.handlebars')
 const onIndexSuccess = (responseData) => {
   $('.results').html('')
   const plantHtml = showPlantsTemplate({ plants: responseData.plants })
-
-  // responseData.plants.forEach(plant => {
-  // //   const plantHtml = `
-  // //     <p>Plant ID: ${plant.id}</p>
-  // //     <h3>${plant.name}</h3>
-  // //     <h4>${plant.location}</h4>
-  // //     <h4>${plant.plant_type}</h4>
-  // //     <hr>
-  // //   `
   $('.results').html(plantHtml)
-  // // })
-  // $('form').trigger('reset')
+  $('form').trigger('reset')
 }
 
 const onShowSuccess = (responseData) => {
