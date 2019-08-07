2f431f05515f99940fd62084eeb33fa1073ae702
elizadavis <eelizadavis@gmail.com>
2019-08-07 08:49:18 -0400

    Add Event Handlers
    Added event handlers for plots, pots

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,6 +8,8 @@
 
 const authEvents = require('./auth/events')
 const plantEvents = require('./plants/events')
+const plotEvents = require('./plots/events')
+const potEvents = require('./pots/events')
 
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
@@ -18,4 +20,11 @@ $(() => {
   $('.results').on('click', '.btn-danger', plantEvents.onDeletePlant)
   $('#plants-create').on('submit', plantEvents.onCreatePlant)
   $('.results').on('submit', '.plants-update', plantEvents.onUpdatePlant)
+  $('.results').on('submit', '.add-plot', plotEvents.onCreatePlot)
+  $('#plots-index').on('click', plotEvents.onGetPlots)
+  $('.plot-results').on('click', '.btn-danger', plotEvents.onDeletePlot)
+  $('.results').on('submit', '.add-pot', potEvents.onCreatePot)
+  $('#pots-index').on('click', potEvents.onGetPots)
+  $('.pot-results').on('click', '.btn-danger', potEvents.onDeletePot)
+  // $('#all-index').on('click', plantEvents.onGetAll)
 })
